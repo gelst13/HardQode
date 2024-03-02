ac58a58d18f676af7f88857dd0593d703c31a90c
Gelst13 <Gelst13@rambler.ru>
2024-03-02 22:11:24 +0300

    working route for course's videos with UserPassesTestMixin to limit access

--- edu/urls.py
+++ edu/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
-# from .views import ProductListView
+from .views import CourseDetailView, VideoListView
 
 
 urlpatterns = [
     path('', views.home, name='edu-home'),
     # path('', ProductListView.as_view(), name='edu-home'),
     path('about/', views.about, name='edu-about'),
-    path('course/<int:pk>', views.course_detail, name='course-detail'),
+    # path('course/<int:pk>', views.course_detail, name='course-detail'),
+    path('course/<int:pk>', CourseDetailView.as_view(), name='course-detail'),
     path('course/<int:pk>/buy', views.buy_course, name='buy-course'),
+    path('course/<int:pk>/video', VideoListView.as_view(), name='course-video'),
 ]
--- edu/views.py
+++ edu/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, redirect
-from django.views.generic import ListView
-from .models import Product, Lesson
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DetailView
+
+from .models import Product, Lesson, Team
 from .forms import BuyForm
 from .utils import EduSys
+
+from users.models import Profile
 
 
 def home(request):
@@ -30,6 +35,46 @@ def course_detail(request, pk):
         'lessons': Lesson.objects.filter(course=Product.objects.get(id=pk)).all()
     }
     return render(request, 'edu/course_detail.html', context)
+
+
+class CourseDetailView(LoginRequiredMixin, DetailView):
+    model = Product
+    fields = ['author', 'title', 'date_msk', 'price', 'min_users', 'max_users', 'number_of_lessons']
+    template_name = 'edu/course_detail.html'
+    context_object_name = 'course'
+
+    # def test_func(self):
+    #     course = self.get_object()
+    #     print(course.title)
+    #     # cluster = course.author[:1] + course.title[:1]
+    #     # if self.request.user.profile.team.name.startswith(cluster):
+    #     if self.request.user.profile.team in Team.objects.filter(course=course).all():
+    #         print('test passed')
+    #         return True
+    #     return False
+
+
+class VideoListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    """Видео-уроки доступны для пользователей, оплативших курс и зачисленных в группу."""
+    model = Lesson
+    fields = ['title', 'video_link', 'course']
+    template_name = 'edu/video_list.html'
+    context_object_name = 'objects'
+    
+    def get_queryset(self):
+        # user = get_object_or_404(User, username=self.kwargs.get('username'))  # kwargs - query params
+        # course = get_object_or_404(Product, id=self.request.user.profile.course.id)
+        course = get_object_or_404(Product, id=self.kwargs.get('pk'))
+        print(course)
+        return Lesson.objects.filter(course=course)  # .all()
+
+    def test_func(self):
+        course = get_object_or_404(Product, id=self.kwargs.get('pk'))
+        print(course)
+        if self.request.user.profile.course == course:
+            print('test passed')
+            return True
+        return False
 
 
 def buy_course(request, pk):
