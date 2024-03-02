2e5dd87ed39acd2238ea677a2061d51dc4cf6e4b
Gelst13 <Gelst13@rambler.ru>
2024-03-02 12:36:31 +0300

    Registered Team model in admin panel

--- edu/admin.py
+++ edu/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product, Lesson
+from .models import Product, Lesson, Team
 
 
 admin.site.register(Product)
 admin.site.register(Lesson)
+admin.site.register(Team)
